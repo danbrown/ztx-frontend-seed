75f71600c5689988e7f2468f779bc0d58af3e381
Daniel Brown <danielbrown25.9.2@gmail.com>
2023-01-26 17:14:16 -0300

    dashboard layout start

--- src/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,18 +1,18 @@
 import { Page, Switch, Flex, PageProps } from "@wipsie/ui";
 import { Head, HeadProps } from "../../components/Head";
-import { DefaultHeader } from "./DefaultHeader";
-import { DefaultFooter } from "./DefaultFooter";
+import { DefaultHeader } from "@layouts/DefaultLayout/DefaultHeader";
+import { DefaultFooter } from "@layouts/DefaultLayout/DefaultFooter";
 
-export type DefaultLayoutProps = PageProps & {
+export type DashboardLayoutProps = PageProps & {
   children: React.ReactNode;
   meta?: HeadProps;
 };
 
-export const DefaultLayout = ({
+export const DashboardLayout = ({
   meta,
   children,
   ...otherProps
-}: DefaultLayoutProps) => {
+}: DashboardLayoutProps) => {
   return (
     <Page backgroundColor="shade" {...otherProps}>
       <Head {...meta} />
