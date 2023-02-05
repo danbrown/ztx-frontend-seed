242248916aad1cf1525b1bccc9396478956fdb7a
Daniel Brown <danielbrown25.9.2@gmail.com>
2023-02-05 04:33:33 -0300

    cleaning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/global.css";
 
 import { Providers } from "../providers";
 
-import { appWithI18Next, useSyncLanguage } from "ni18n";
+import { appWithI18Next } from "ni18n";
 import { ni18nConfig } from "../../ni18n.config";
 
 const MyApp: React.FC<any> = ({ Component, pageProps }) => {
